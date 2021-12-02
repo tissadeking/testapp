9bba59fea80bdce0470d735dd52c79aa14eb7959
tissadeking <tissadeking@yahoo.com>
2021-12-02 14:36:08 +0100

    changes to index.html

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
-//const option_list = document.querySelector(".option_list");
+
 const time_line = document.querySelector("header .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
